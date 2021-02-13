56646916049498f5ee0950d562344cf5fc82f69e
MehediHasnaHira <52360118+MehediHasnaHira@users.noreply.github.com>
2021-02-13 13:29:14 +0600

    here it is a basic setup screen
    here it is a basic setup screen a image view and icon

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Macbook Pro 2015 on 11/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
